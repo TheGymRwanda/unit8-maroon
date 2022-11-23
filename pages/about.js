a25d30b72d7b135d8ad12bebd0daf61506c30abe
haroldmud <haroldmudosa@gmail.com>
2022-11-23 16:44:18 +0200

    Fix: Removing about in button component

--- pages/about.js
+++
@@ -1,9 +0,0 @@
-import AboutMobile from "../components/section/AboutSection/AboutMobile";
-
-export default function about(){
-    return(
-        <>
-        <AboutMobile />
-        </>
-    )
-}
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,7 @@ import Button from "../components/ui/Button";
 
 export default function Home() {
   return (
-    <div className="">
-    
-     
+    <div className=""> 
       <Button text="testing" styles=""/>
     </div>
   );
