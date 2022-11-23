78b16bbf9669ffd9a21906df7d481a108c4c67a8
Delyc <d.twizeyima@alustudent.com>
2022-11-23 14:57:11 +0200

    feat: Creating button component

--- components/ui/Button.jsx
+++ components/ui/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({ text, styles }) {
   return (
     <button
-      className={`bg-orange-bg font-Neufile  text-2xl font-normal not-italic leading-7  text-dark-grey  ${styles} `}>
+      className={`font-Neufile  text-2xl font-normal not-italic leading-7  text-dark-grey  ${styles} `}>
       {text}
     </button>
   );
