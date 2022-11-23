9641b003f833f31232c101278129adeef60c0ef5
Delyc <d.twizeyima@alustudent.com>
2022-11-23 17:45:47 +0200

    feat: About section on mobile

--- components/functions/OneBlock.js
+++ components/functions/OneBlock.js
@@ -1,6 +1,6 @@
 
 import Image from 'next/image'
-import upArrow from '../../public/assets/icons/up-arrow.png'
+import downArrow from '../../public/assets/icons/down-arrow.png'
 export function OneBlock({
     title,
     text,
@@ -8,11 +8,13 @@ export function OneBlock({
     expand,
   }) {
     return (
-      <div className='flex flex-col'>
+      <div className='flex flex-col '>
         <div
           onClick={() => expand()}
-          className={`flex w-full cursor-pointer justify-between border-b-1 ${isOpened ? 'border-primary-purple' : 'border-solid-black'} `}
+          className={`flex w-full cursor-pointer justify-between border-b-1 ${isOpened ? 'border-b border-primary-purple' : 'border-b border-solid-black'} `}
         >
+
+          
           <h2 className={`${
             isOpened ? 'text-primary-purple' : 'text-black'
           } w-full cursor-pointer  `}
@@ -22,13 +24,13 @@ export function OneBlock({
               isOpened ? 'rotate-180' : 'rotate-0'
             } mb-1 cursor-pointer transition-all duration-300 ease-in`}
           >
-            <Image src={upArrow} height={20} width={20} alt='Arrow' />
+            <Image src={downArrow} height={11} width={11.53} alt='Arrow' />
           </div>
         </div>
   
         <p
           className={`${
-            isOpened ? 'h-max opacity-100' : 'h-0 opacity-0'
+            isOpened ? 'h-auto opacity-100' : 'h-0 opacity-0'
           } transition-all duration-300 ease-in`}
         >
           {text}
---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import AboutMobile from "../components/section/AboutSection/AboutMobile";
+
+export default function About(){
+    return(
+        <AboutMobile />
+    )
+}
