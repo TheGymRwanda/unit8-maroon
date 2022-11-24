629948dcd5a03bbd46e3038fb0688b2cfe4338d1
Nkaka23dev <nkakaeric96@gmail.com>
2022-11-24 17:11:42 +0200

    feat: worked on card component for all devices

--- components/cards/BlogCard.jsx
+++ components/cards/BlogCard.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 
-export default function BlogCard() {
+export default function BlogCard(props) {
   return (
     <>
-      <section className="box-border  flex h-102.5 w-75.5 flex-col gap-6 rounded-xl border border-solid border-secondaryGrey p-5 drop-shadow-2.5xl ipad:w-112 ipad:h-96.5  ">
-        <p className="font-Neufile text-lg  leading-2.5  tracking-cardDescription text-primaryGrey w-65.5 h-4.5 ipad:w-102  ">
-          02 Oct 2022
+      <section className="box-border flex h-102.5 w-75.5 flex-col gap-6 rounded-xl border border-solid border-secondaryGrey p-5 drop-shadow-2.5xl desktop:px-35 desktop:gap-38 ipad:w-112 desktop:w-29.8125 ipad:h-96.5 desktop:h-25.0869">
+        <p className="font-Neufile leading-2.5 tracking-cardDescription text-primaryGrey w-65.5 h-4.5 ipad:w-102 font-normal desktop:w-27.3125 text-desktopHeadingTwo">
+          {props.children}
         </p>
 
-        <h2 className="h-17  w-65.5 font-Neufile text-mobHeadingThree font-normal leading-8.5 ipad:h-21 ipad:w-109.25 ipad:text-4xl ipad:leading-10.5  tracking-cardDescription text-black ">
-          Launch of the Africa DeFi Alliance DAO
+        <h2 className="h-17 w-65.5 font-Neufile text-mobHeadingThree font-normal leading-8.5 ipad:h-21  ipad:w-109.25  ipad:text-4xl  ipad:leading-10.5  tracking-cardDescription text-black desktop:text-tabletHeadingOne tablet:text-tabletHeadingOne desktop:leading-2.625 tablet:leading-2.6">
+        {props.children}
         </h2>
 
-        <div className="h-42 w-65.5 font-Neufile text-xl font-normal not-italic tracking-cardSubTitle text-black ipad:w-101.25 ipad:h-40 ipad:text-2xl ipad:tracking-cardSubDescription ">
-          An auspicious group of like-minded partners will deploy an open
-          protocol for decentralized working <br /> capital and MSME financial
-          solutions across Africa.
+        <div className="w-65.5 h-42 font-Neufile text-xl font-normal tracking-cardSubTitle text-black ipad:w-101.25 ipad:h-40 ipad:text-2xl ipad:tracking-cardSubDescription desktop:w-27.3125 desktop:h-8">
+        {props.children}
         </div>
       </section>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,13 +17,13 @@ module.exports = {
       // 'desktop': '1440px',
     },
     extend: {
-      colors:{
-        "primaryPurple" : "#5602E0",
-        "primaryGrey":"#878787",
-        "secondaryGrey":"#EDEDED",
-        "darkGrey" : "#9A9A9A",
-        "lightGrey" : "#E4E4E4",
-        
+      colors: {
+        "primaryPurple": "#5602E0",
+        "primaryGrey": "#878787",
+        "secondaryGrey": "#EDEDED",
+        "darkGrey": "#9A9A9A",
+        "lightGrey": "#E4E4E4",
+
       },
       letterSpacing: {
         "spacing-xl": '-0.02rem',             //-2%
@@ -31,65 +31,76 @@ module.exports = {
         "spacing-md": '-0.015rem',            //-1.5%
         "spacing-sm": '-0.0125rem',           //-1.25%
       },
-      fontSize:{
-        "headingOne" : "4rem",               //64px
-        "mobHeadingOne" : "3.5rem",         //56px
-        "mobHeadingThree" : "1.75rem"       //28px
+      fontSize: {
+        "headingOne": "4rem",               //64px
+        "mobHeadingOne": "3.5rem",         //56px
+        "mobHeadingThree": "1.75rem",
+        "tabletHeadingOne": "2.25rem",
+        "desktopHeadingTwo": "1.125rem"
       },
 
-      lineHeight:{
-        "18" : "4.5rem",             //72px
-        "10.5" :"2.625rem",                  //42px
-        "4.5" : "1.125rem",             //18px
+      lineHeight: {
+        "18": "4.5rem",             //72px
+        "10.5": "2.625rem",                  //42px
+        "4.5": "1.125rem",             //18px
         '8.5': '2.125rem',              //34px
-        "2.5":"100%",
-       
+        "2.5": "100%",
+        "2.6": "2.625rem",
+        "2.625": "2.625rem"
       },
       fontFamily: {
-        'Neufile': ['Neufile', 'sans-serif'],        
+        'Neufile': ['Neufile', 'sans-serif'],
       },
-      spacing:{
-        "24.5" : "6.1513rem", //98.4208px
-        
+      spacing: {
+        "24.5": "6.1513rem", //98.4208px
+        "38": "2.375rem"
       },
-      width:{
-        "11.5/12" : "91%",      // width on desktop
-        "11.8/12" : "93%",      //width on tablet
-        "10.5/12" : "90%",      //width on mobile
-        "112": '28rem',         // width 448px
+      padding: {
+        '35': '2.1875rem',
+      },
+      width: {
+        '11.5/12': '91%',   // width on desktop
+        '11.8/12': '93%',     //width on tablet
+        '10.5/12': '90%',     //width on mobile
+        '112': '28rem',         // width 448px
         '75.5': '18.875rem',
         '65.5': '16.375rem',
         '102': '25.5rem',
         '109.25': '27.313rem',
         '101.25': '25.313rem',
+        '29.8125': '29.8125rem',
+        '27.3125': '27.3125rem',
+        
       },
 
-      height:{
+      height: {
         '96.5': '24.125rem',
         '102.5': '25.625rem',
         '17': '4.25rem',
         '42': '10.5rem',
         '4.5': '1.125rem',
         '21': '5.25rem',
+        '25.0869': '25.0869rem',
+        '8': '8rem'
       },
 
-      inset:{
+      inset: {
         '736': '184rem',
       },
       letterSpacing: {
         "cardTitle": '-0.02em',
         "cardDescription": "-0.01em",
-        "cardSubDescription":"-0.015em",
-        "cardSubTitle":"-0.0125em"
+        "cardSubDescription": "-0.015em",
+        "cardSubTitle": "-0.0125em"
       },
       dropShadow: {
         '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
         '4xl': [
-            '0 35px 35px rgba(0, 0, 0, 0.25)',
-            '0 45px 65px rgba(0, 0, 0, 0.15)'
+          '0 35px 35px rgba(0, 0, 0, 0.25)',
+          '0 45px 65px rgba(0, 0, 0, 0.15)'
         ],
 
-         "2.5xl":"1px 2px 5px rgba(0, 0, 0, 0.25)"
+        "2.5xl": "1px 2px 5px rgba(0, 0, 0, 0.25)"
       }
     },
   },
