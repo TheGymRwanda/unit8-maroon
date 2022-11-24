07ea1f44e78fe77c2a7e4fd964a4608c0373cef5
Delyc <d.twizeyima@alustudent.com>
2022-11-24 18:37:06 +0200

    chore: Removing hover effect since not all buttons will have it

--- components/ui/Button.jsx
+++ components/ui/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button(props) {
   return (
     <button
-      className={`font-Neufile  text-2xl font-normal not-italic leading-7  text-dark-grey hover:text-primary-purple ${props.style} `}>
+      className={`font-Neufile  text-2xl font-normal not-italic leading-7  text-dark-grey  ${props.style} `}>
       {props.children}
     </button>
   );
