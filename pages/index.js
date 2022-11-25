8cf04ba26dfa35b0d9aa31ddfc6afe27c6b829e9
Delyc <d.twizeyima@alustudent.com>
2022-11-25 11:39:22 +0200

    feat: Renaming UI to ui

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import Link from "../components/UI/Link";
-import Svg from "../components/UI/Svg";
+import Link from "../components/ui/Link";
+import Svg from "../components/ui/Svg";
 export default function Home() {
   return (
     <body>
-      <Link text={"Contact"} textStyle="text-black" arrow={<Svg />} imageWrapperStyle="absolute -top-2"/>
+      <Link
+        text={"Contact"}
+        textStyle="text-black"
+        arrow={<Svg />}
+        imageWrapperStyle="absolute -top-2"
+      />
     </body>
   );
 }
