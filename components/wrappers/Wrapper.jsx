1556dcfa198d1a591547ddf71faf43fa1edd4f23
ishoborabyose <ishoborabyosebeatrice@gmail.com>
2022-11-28 12:13:12 +0200

    Feat: Wrapper component

--- components/wrappers/Wrapper.jsx
+++ components/wrappers/Wrapper.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-export default function Wrapper(children, styles) {
+export default function Wrapper({ children, styles }) {
   return (
     <div
-      className={`pl-1.2813 ipad:pl-2.25 pb-24 ipad:pb-10 desktop-sm:pb-5 ipad:pr-2.4375 desktop:w-83 desktop:mx-auto tablet:px-3.85 desktop:px-0 ${styles}`}
+      className={`px-5 pl-1.2813  pb-24 ipad:pl-2.25 ipad:pb-10 ipad:pr-2.4375 tablet:px-3.85 desktop-sm:pb-5 desktop:mx-auto desktop:w-83 desktop:px-0 ${styles}`}
     >
-     
       {children}
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,11 @@
-// import Link from "../components/ui/Link";
-
 import Wrapper from "../components/wrappers/Wrapper";
-
-// import Svg from "../components/ui/Svg";
 export default function Home() {
   return (
-    <body>
-      {/* <Link
-        text={"Contact"}
-        textStyle=""
-        arrow={<Svg />}
-        imageWrapperStyle="absolute -top-0.5"
-      /> */}
-
-      <Wrapper />
-    </body>
+<Wrapper>
+<div>
+I have recently built and uploaded online my first website for a client. I have one slight problem, when you hover over the bottom three buttons on the index page, the images disappear momentarily until hovered over again. This also happens on the gallery page. I have no clue as to why it is doing this. I have declared a background image for a then a different one for the a:hover.
+</div>
+</Wrapper>
+  
   );
 }
