dba2dd4db400d15fe3b1df1776c8ad914d8f7f83
ishoborabyose <ishoborabyosebeatrice@gmail.com>
2022-11-28 16:38:33 +0200

    Feat: Wrapper component

--- components/wrappers/Wrapper.jsx
+++ components/wrappers/Wrapper.jsx
@@ -4,7 +4,7 @@ export default function Wrapper({ children, styles }) {
   return (
     <section className="flex justify-center ]">
         <div
-        className={`max-w-90 w-10.5/12 md:w-11.8/12 lg:w-11.5/12   ${styles}`}
+        className={`max-w-90 w-10.5/12 md:w-11.5/12 lg:w-11.8/12 xl:w-11.6/12  ${styles}`}
       >
         {children}
       </div>
