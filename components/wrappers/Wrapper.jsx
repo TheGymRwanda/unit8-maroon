4fbd0aefaa20b28f809170d9b58a82dc3648fadf
ishoborabyose <ishoborabyosebeatrice@gmail.com>
2022-11-28 15:22:53 +0200

    Feat: Wrapper component

--- components/wrappers/Wrapper.jsx
+++ components/wrappers/Wrapper.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Wrapper({ children, styles }) {
   return (
     <div
-      className={`px-5 pl-1.2813  pb-24 ipad:pl-2.25 ipad:pb-10 ipad:pr-2.4375 tablet:px-3.85 desktop-sm:pb-5 desktop:mx-auto desktop:w-83 desktop:px-0 ${styles}`}
+      className={`px-5   ipad:px-2.25  tablet:px-2.25  desktop:mx-auto desktop:w-83 desktop:px-0 ${styles}`}
     >
       {children}
     </div>
