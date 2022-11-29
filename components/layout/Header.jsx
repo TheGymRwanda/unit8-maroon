197c7dd699962a9257ebbed3bd9833908bf4033e
Nkaka23dev <nkakaeric96@gmail.com>
2022-11-29 09:01:59 +0200

    Fix: Header is fixed on all breakpoints

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -12,23 +12,36 @@ export default function Header() {
         <div className="xs:w-[21.875rem] h-[15.75rem] text-xl md:text-2xl">
           Unit➇ is a pioneering technology and marketing services company that
           creates end-to-end digital experiences for brands such as{" "}
-          <span className="inline-block text-primary-purple lg:hidden">
+          <span className="inline-block text-primary-purple md:hidden">
             Google,{" "}
           </span>{" "}
-          <span className="inline-block text-primary-purple lg:hidden">
+          <span className="inline-block text-primary-purple md:hidden">
             philips,{" "}
           </span>
-          <span className="inline-block text-primary-purple lg:hidden">
+          <span className="inline-block text-primary-purple md:hidden">
             Audi,
           </span>{" "}
-          <span className="inline-block text-primary-purple lg:hidden">
+          <span className="inline-block text-primary-purple md:hidden">
             Twitch,
           </span>{" "}
-          <span className="inline-block text-primary-purple lg:hidden">
+          <span className="inline-block text-primary-purple md:hidden">
             Patagonia,
           </span>{" "}
-          <span className="inline-block text-primary-purple lg:hidden">
+          <span className="inline-block text-primary-purple md:hidden">
             eBay
+          </span>{" "}
+          {/* style for Ipad */}
+          <span className="hidden text-primary-purple md:inline-block">
+            Ethereum,
+          </span>{" "}
+          <span className="hidden text-primary-purple md:inline-block">
+            Tezos,
+          </span>{" "}
+          <span className="hidden text-primary-purple md:inline-block">
+            Near,
+          </span>{" "}
+          <span className="hidden text-primary-purple md:inline-block">
+            Algorand
           </span>{" "}
           and more. Its team of 2,500+ digital specialists across 30+
           locations on 5 continents delivers pioneering work on a global scale
@@ -38,34 +51,37 @@ export default function Header() {
           </span>
         </div>
       </div>
-      {/* Ipads */}
+       {/* Ipad */}
+      {/* desktop */}
       <div className="hidden lg:block">
-        <div className="xs:w-[21.875rem] h-[15.75rem] text-xl md:text-2xl">
-          Unit➇ is a pioneering technology and marketing services company that
-          creates end-to-end digital experiences for brands such as{" "}
-          <span className="inline-block text-primary-purple ">
-            Google,{" "}
+        <div className="xs:w-[21.875rem] xl:w-[47.3125rem] h-[15.75rem] text-xl md:text-2xl">
+          {/* lg:w-[47.3125rem]  xl:w-7/13 */}
+          {/* justify-between lg:flex */}
+          Unit➇ is a pioneering technology company specialing in decentralised
+          technologies that creates end-to-end digital experiences for
+          protocols including{" "}
+          <span className="hidden text-primary-purple md:inline-block">
+            Ethereum,
           </span>{" "}
-          <span className="inline-block text-primary-purple ">
-            philips,{" "}
-          </span>
-          <span className="inline-block text-primary-purple ">
-            Audi,
+          <span className="hidden text-primary-purple md:inline-block">
+            Tezos,
           </span>{" "}
-          <span className="inline-block text-primary-purple ">
-            Twitch,
+          <span className="hidden text-primary-purple md:inline-block">
+            Near,
           </span>{" "}
-          <span className="inline-block text-primary-purple ">
-            Patagonia,
-          </span>{" "}
-          <span className="inline-block text-primary-purple ">
-            eBay
+          <span className="hidden text-primary-purple md:inline-block">
+            Algorand
           </span>{" "}
           and more. Its team of 2,500+ digital specialists across 30+
           locations on 5 continents delivers pioneering work on a global scale
-          with a boutique culture.
-          <span className="inline-block">
-            <PageLink text={"Contact"} arrow={<Svg />} />
+          with a boutique culture.{" "}
+          <span className="hidden lg:inline-block">
+            <PageLink
+              text={"Contact"}
+              arrow={<Svg />}
+              textStyle=" underline"
+              style="xl:flex xl:gap-1/2 gap-0.5  "
+            />
           </span>
         </div>
       </div>
