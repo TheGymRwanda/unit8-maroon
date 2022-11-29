c959a8d6bed3b1599c64bfc44f792708f8e131c9
Delyc <d.twizeyima@alustudent.com>
2022-11-29 17:43:59 +0200

    feat: Adding border on about text

--- components/functions/OneTab.jsx
+++ components/functions/OneTab.jsx
@@ -7,7 +7,7 @@ export function OneTab({
       <span
         onClick={() => setActive()}
         className={`cursor-pointer transition-all duration-300 ease-in  hover:text-primary-purple text-2xl mb-5 ${
-          isActive ? 'text-primary-purple underline' : 'text-dark-grey'
+          isActive ? 'text-primary-purple border-b-2 border-primary-purple' : 'text-dark-grey'
         }`}
       >
         {text}
