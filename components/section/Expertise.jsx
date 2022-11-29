2e07d744b7a103b2b2d4e96b15900b82486ce3e6
ishoborabyose <ishoborabyosebeatrice@gmail.com>
2022-11-29 08:30:01 +0200

    Feat: Expertise section

--- components/section/Expertise.jsx
+++ components/section/Expertise.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Wrapper from "../wrappers/Wrapper";
 
 export default function Expertise() {
   const firstList = [
@@ -16,40 +17,47 @@ export default function Expertise() {
     "Product Design",
   ];
   return (
-    <section className="float-right">
-      <div className="flex  flex-col gap-0.4375 text-left ipad:gap-6 desktop:gap-6">
-        <p className="font-Neufile  text-xl  font-normal tracking-spacing-sm text-expertiseGrey  ipad:text-1.5 desktop:text-1.5">
-          Expertise
-        </p>
+    <Wrapper>
+   <section className="float-right">
+        <div className="flex  flex-col gap-0.4375 text-left ipad:gap-6 desktop:gap-6">
+          <p className="font-Neufile  text-xl  font-normal tracking-spacing-sm text-expertiseGrey  ipad:text-1.5 desktop:text-1.5">
+            Expertise
+          </p>
 
-        <div className="flex flex-col gap-2 ipad:grid ipad:grid-cols-2   ipad:gap-3 ipad:space-x-6 desktop:gap-3">
-          <div className="flex flex-col gap-2 ipad:gap-3 desktop:gap-3  ">
-            {firstList.map((list, i) => {
-              return (
-                <div key={i}>
-                  <p className="font-Neufile text-xl font-normal tracking-spacing-sm text-black ipad:text-2xl">
-                    {list}
-                  </p>
-                  <hr className="solid w-16.0625 border-borderGrey ipad:w-18.1306  desktop:w-19.375" />
-                </div>
-              );
-            })}
-          </div>
+          <div className="flex flex-col gap-2 ipad:gap-3 desktop:gap-3   ipad:grid ipad:grid-cols-2 ipad:space-x-6">
+           
 
-          <div className="flex flex-col gap-2 ipad:gap-3 desktop:gap-3 ">
-            {secondList.map((list, i) => {
-              return (
-                <div key={i}>
-                  <p className="font-Neufile text-xl font-normal tracking-spacing-sm text-black ipad:text-2xl">
-                    {list}
-                  </p>
-                  <hr className="solid w-16.0625 border-borderGrey ipad:w-18.1306  desktop:w-19.375" />
-                </div>
-              );
-            })}
+            <div className="flex flex-col gap-2 ipad:gap-3 desktop:gap-3  ">
+              {firstList.map((list, i) => {
+                return (
+                  <div key={i}>
+                    <p className="font-Neufile text-xl font-normal tracking-spacing-sm text-black ipad:text-2xl">
+                      {list}
+                    </p>
+                    <hr className="solid w-16.0625 border-borderGrey ipad:w-18.1306  desktop:w-19.375" />
+                  </div>
+                );
+              })}
+            </div>
+
+            <div className="flex flex-col gap-2 ipad:gap-3 desktop:gap-3 ">
+              {secondList.map((list, i) => {
+                return (
+                  <div key={i}>
+                    <p className="font-Neufile text-xl font-normal tracking-spacing-sm text-black ipad:text-2xl">
+                      {list}
+                    </p>
+                    <hr className="solid w-16.0625 border-borderGrey ipad:w-18.1306  desktop:w-19.375" />
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Wrapper>
+   
+   
+
   );
 }
