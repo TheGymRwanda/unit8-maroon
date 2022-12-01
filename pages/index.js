dbb6f2c56445aefc0ab3e1310337236bfde4c1b2
Delyc <d.twizeyima@alustudent.com>
2022-12-01 17:46:03 +0200

    fix: Fixing gaps in project and adding about in index

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Project from "../components/section/Project";
 import Footer from "../components/layout/Footer";
 import Header from "../components/layout/Header";
 import Expertise from "../components/section/Expertise";
+import About from "../components/section/AboutSection/About";
 export default function Home() {
   return (
     <>
       <Header />
       <Project />
       <Expertise />
+      <About />
       <Footer />
     </>
   );
