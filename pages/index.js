e4d2a516e875a306ae84ba53a9880f3b05a59e3a
ishoborabyose <ishoborabyosebeatrice@gmail.com>
2022-12-01 11:30:51 +0200

    Fix: fixed changes in expertise

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,14 @@ import Wrapper from "../components/wrappers/Wrapper";
 import Expertise from "../components/section/Expertise";
 import Footer from "../components/layout/Footer";
 import Header from "../components/layout/Header";
-
-import Wrapper from "../components/wrappers/Wrapper";
 import About from '../components/section/AboutSection/About'
 
 export default function Home() {
   return (
 <Wrapper>
-<Header/>
+    <Header/>
     <Expertise />
     <About />
-
-
     <Footer/>
 </Wrapper>
   
