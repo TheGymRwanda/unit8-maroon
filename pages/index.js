c5df4a13f0a375a93e40c34eb5a9688a6895433f
Delyc <d.twizeyima@alustudent.com>
2022-12-01 16:57:31 +0200

    fix: Fixing conflicts

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
+// import Project from "../components/section/Project";
+// import Footer from "../components/layout/Footer";
+// import Header from "../components/layout/Header";
+// import Wrapper from "../components/wrappers/Wrapper";
+// import Expertise from "../components/section/Expertise";
+// export default function Home() {
+
+//   return (
+//     <>
+//       <Header />
+//       <Project />
+//       <Expertise />
+//       <Footer />
+//     </>
+//   );
+// }
+
+
+
 import Project from "../components/section/Project";
 import Footer from "../components/layout/Footer";
 import Header from "../components/layout/Header";
-import Wrapper from "../components/wrappers/Wrapper";
 import Expertise from "../components/section/Expertise";
 export default function Home() {
-
   return (
     <>
       <Header />
