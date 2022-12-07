08f8d78ebfa72beb17809fd24c86a2016a5c2210
ishoborabyose <ishoborabyosebeatrice@gmail.com>
2022-12-07 11:11:39 +0200

    Fix: fixing bugs on expertise section

---
+++ dataCollection/data.js
@@ -0,0 +1,12 @@
+export const expertiseList = [
+    "Product Development",
+    "Community management",
+    "Governance",
+    "Product Development",
+    "Product Development",
+    "Product Design",
+    "Strategy",
+    "Product Design",
+    "Product Design",
+    "Product Design",
+  ];
