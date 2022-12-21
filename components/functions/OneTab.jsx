5fea04e588d03aba9a19349ee2b537d0aed25c4c
John U <arjorb@gmail.com>
2022-12-21 13:24:46 +0200

    removing spaces on oneTab Component

--- components/functions/OneTab.jsx
+++ components/functions/OneTab.jsx
@@ -2,7 +2,7 @@ export function OneTab({ text, isActive, setActive }) {
   return (
     <span
       onClick={() => setActive()}
-      className={`mb-5   cursor-pointer text-2xl ${
+      className={`mb-5 cursor-pointer text-2xl ${
         isActive ? 'border-b-2 border-primary-purple text-primary-purple' : 'text-dark-grey'
       }`}
     >
