87ff4421ea16d64763801b83e7fcf30e19fe82b2
John U <arjorb@gmail.com>
2022-12-22 11:37:01 +0200

    Done fixing the header arrow hover to dissapear the underline

--- components/ui/HeaderLink.jsx
+++ components/ui/HeaderLink.jsx
@@ -1,15 +1,14 @@
-import PageLink from "./Link";
-import Svg from "./Svg";
+import PageLink from './Link';
+import Svg from './Svg';
 
-
-export default function HeaderLink(){
-  return(
+export default function HeaderLink() {
+  return (
     <PageLink
-      style="gap-2"
-      link={"mailto:web3@apeunit.com"}
-      text={"Contact"}
-      textStyle="no-underline md:underline md:hover:no-underline"
+      style='gap-2'
+      link={'mailto:web3@apeunit.com'}
+      text={'Contact'}
+      textStyle='no-underline md:underline md:group-hover:no-underline'
       arrow={<Svg />}
     />
-  )
+  );
 }
